a885e5269abff9d276b147701f7810aa0c709327
TatyanaSerK <163410714+TatyanaSerK@users.noreply.github.com>
2024-04-11 14:41:36 +0300

    Update main.py
    добавила комментарии

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-class home:
-    numberOfFloors = 10
+class home:                # зададим класс дом
+    numberOfFloors = 10    # количество этажей в доме
 
-    def lift(self):
-        Floor = self
-        while home.numberOfFloors != Floor:
-            Floor += 1
-            print('Текущий этаж равен: ', Floor)
+    def lift(self):                                  # создадим функцию лифт(для перебора этажей)
+        Floor = self                                 # создадим переменную этаж
+        while Floor != home.numberOfFloors:          # пока этаж не равен макс значению
+            Floor += 1                               # прибавим 1 этаж
+            print('Текущий этаж равен: ', Floor)     # выводим текущее значение этажа
 
 
-home.lift( 0 )
+home.lift(0)
